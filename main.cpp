d261774b7666a13ad1d957c5daf09b92318667b1
Kamil <keimil00@gmail.com>
2022-04-15 21:02:14 +0200

    Changed functions to lambdas

--- main.cpp
+++ main.cpp
@@ -12,26 +12,26 @@ using namespace std;
 
 using Collection = vector<shared_ptr<Shape>>;
 
-bool sortByArea(shared_ptr<Shape> first, shared_ptr<Shape> second)
+auto sortByArea = [](shared_ptr<Shape> first, shared_ptr<Shape> second)
 {
     if(first == nullptr || second == nullptr)
         return false;
     return (first->getArea() < second->getArea());
-}
+};
 
-bool perimeterBiggerThan20(shared_ptr<Shape> s)
+auto perimeterBiggerThan20 = [](shared_ptr<Shape> s)
 {
     if(s)
         return (s->getPerimeter() > 20);
     return false;
-}
+};
 
-bool areaLessThan10(shared_ptr<Shape> s)
+auto areaLessThanX = [x = 10](shared_ptr<Shape> s)
 {
     if(s)
-        return (s->getArea() < 10);
+        return (s->getArea() < x);
     return false;
-}
+};
 
 void printCollectionElements(const Collection& collection)
 {
@@ -48,7 +48,7 @@ void printAreas(const Collection& collection)
 }
 
 void findFirstShapeMatchingPredicate(const Collection& collection,
-                                     bool (*predicate)(shared_ptr<Shape> s),
+                                     std::function<bool(shared_ptr<Shape>)> predicate,
                                      std::string info)
 {
     auto iter = std::find_if(collection.begin(), collection.end(), predicate);
@@ -81,13 +81,13 @@ int main()
 {
     //constexpr auto result = fib(45);  //Too big number of steps for clang, and I don't really know how to change that
     Collection shapes = {
-            make_shared<Circle>(2.0),
-            make_shared<Circle>(2.0),
+            make_shared<Circle>(RED, 2.0),
+            make_shared<Circle>(GREEN, 2.0),
             nullptr,
-            make_shared<Circle>(4.0),
-            make_shared<Rectangle>(10.0, 5.0),
-            make_shared<Square>(3.0),
-            make_shared<Circle>(4.0),
+            make_shared<Circle>(BLUE, 4.0),
+            make_shared<Rectangle>(BLUE, 10.0, 5.0),
+            make_shared<Square>(BLUE, 3.0),
+            make_shared<Circle>(GREEN, 4.0),
             make_shared<Square>(RED)
     };
 
@@ -103,11 +103,13 @@ int main()
     cout << "Areas after sort: " << std::endl;
     printAreas(shapes);
 
-    auto square = make_shared<Square>(4.0);
+    auto square = make_shared<Square>(RED, 4.0);
     shapes.push_back(square);
 
+
+
     findFirstShapeMatchingPredicate(shapes, perimeterBiggerThan20, "perimeter bigger than 20");
-    findFirstShapeMatchingPredicate(shapes, areaLessThan10, "area less than 10");
+    findFirstShapeMatchingPredicate(shapes, areaLessThanX, "area less than 10");
 
     return 0;
 }
